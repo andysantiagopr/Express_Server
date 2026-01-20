d4b97aecd5cf682bbf445ce48598aba7f7762603
andysantiagopr <andysantiagopr@gmail.com>
2026-01-20 13:26:18 -0800

    Changed Morgan middleware code on top to avoid login issue

--- server/app.js
+++ server/app.js
@@ -6,14 +6,14 @@ this is splitted from index.js to be able to test without a server, reuse app, a
 */
 
 // import files and packages up here
-const express = require("express");
-const app = express();
-const morgan = require('morgan');
-const data = require('./data.json');
-// create your express server below
-
+const express = require("express"); //imports express library (blueprint)
+const app = express(); //creates a server app using Express (building)
+const morgan = require('morgan'); //imports the morgan library. Middleware for Express that helps log HTTP requests.
+const data = require('./data.json'); //Loads the local json file and stores it in data.
 
 // add your routes and middleware below
+
+app.use(morgan('tiny')); //runs Morgan middleware for every incoming req using the tiny logging format. 
 
 app.get("/", (req, res) => { //app.get registers a route. This defines a route for the route path of the server
   res.send("It's working"); //sets the response. Ends the request. 
@@ -23,7 +23,7 @@ app.get('/data', (req, res) => { //This creates a route so that when someone sen
   res.json(data);
 });
 
-app.use(morgan('tiny')); //runs Morgan middleware for every incoming req using the tiny logging format. 
+
 
 // finally export the express application
 
